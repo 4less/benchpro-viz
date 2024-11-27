65b58b2098c5b2bf9edb43487791a283ed2c34fe
4less <joachim.fritscher@gmail.com>
2024-11-27 21:12:37 +0000

    trying to deploy static website to github.io

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  base: '/benchpro-viz/',
 })
